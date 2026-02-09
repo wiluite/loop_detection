7d6c95d1e9e31a3b1b58d261000c361a9137d138
wiluite <tetasoft@yandex.ru>
2026-02-09 11:42:19 +0300

    Fix Doc and main module.

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
-#if 0
+#if 1
 #include "solution1.h"
 #else
 #include "solution2.h"
 #endif
+#include <cassert>
 
 int main() {
     try {
-        int break_cycle_after = 20;
+        int break_cycle_after = 200;
         for (auto e : looped_forward_list<int, 6, 2> {12, 14, 16, 18, 20, 22, 24, 26}) {
             std::cout << e << ' ';
             if (!--break_cycle_after)
                 break;
         }
         std::cout << std::endl;
-        std::cout << std::boolalpha << has_cycle(looped_forward_list<int, 6, 2>{12, 14, 16, 18, 20, 22, 24, 26}) << std::endl;
-        std::cout << std::boolalpha << has_cycle(looped_forward_list<int, 2, 6>{12, 14, 16, 18, 20, 22, 24, 26}) << std::endl;
-        std::cout << std::boolalpha << has_cycle(std::forward_list<int>{12, 14, 16, 18, 20, 22, 24, 26}) << std::endl;
+        assert (has_cycle(looped_forward_list<int, 6, 2>{12, 14, 16, 18, 20, 22, 24, 26}));
+        assert (!has_cycle(looped_forward_list<int, 2, 6>{12, 14, 16, 18, 20, 22, 24, 26}));
+        assert (!has_cycle(std::forward_list<int>{12, 14, 16, 18, 20, 22, 24, 26}));
     } catch(std::exception const & e) {
         std::cout << e.what() << std::endl;
     }
